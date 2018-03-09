342b7a3d06c309af210313a39045dd283f36e951
José S R <37203999+JoseSanchezReyes@users.noreply.github.com>
2018-03-09 01:03:37 -0600

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BLOGDeportes.Startup))]
+namespace BLOGDeportes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
